18416a3b3adbe878e53fbd4dc4067be3544f7776
ChristianHopf <cehopf@gmail.com>
2025-05-12 17:20:48 -0400

    Basic sidebar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,11 +2,9 @@ import { Menu, User } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen grid-cols-1">
-      <header className="flex text-center justify-between border-b">
-        <h1 className="text-2xl font-semibold px-4 md:px-6">WORLDVIEW</h1>
-      </header>
-      <div></div>
+    <div className="min-h-screen w-full">
+      <main></main>
+
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
---
+++ frontend/components/app-sidebar.tsx
@@ -0,0 +1,53 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Home, Settings } from "lucide-react";
+
+const items = [
+  {
+    title: "Servers",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Worldview</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
