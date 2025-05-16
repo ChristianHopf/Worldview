51d911d255e955a49a9cb4f88d683b923b8f9fb6
ChristianHopf <cehopf@gmail.com>
2025-05-16 12:43:51 -0400

    Add page components

---
+++ frontend/app/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+  return (
+    <div className="min-h-screen w-full">
+      <h1>Settings stuff goes here</h1>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+    </div>
+  );
+}
