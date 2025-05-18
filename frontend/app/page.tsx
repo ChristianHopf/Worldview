27e37ffe6ccd7cffd74a3416d313ef33a7f53f71
ChristianHopf <cehopf@gmail.com>
2025-05-18 16:55:49 -0400

    Changed home page to grid

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,17 @@
+import ServerHealth from "@/components/home/server-health";
 import ServerLogs from "@/components/home/server-logs";
 import ServerStatus from "@/components/home/server-status";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full">
-        <div>
-          <h1 className="text-3xl">Metrics</h1>
-          <div className="flex flex-row">
-            <ServerStatus />
-          </div>
-        </div>
-        <div className="flex flex-row w-full">
-          <ServerLogs />
-        </div>
-
+    <div className="min-h-screen w-full flex flex-col gap-4 p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ServerStatus />
+        <ServerHealth />
+      </div>
+      <div className="grid grid-cols-1 gap-4">
+        <ServerLogs />
+      </div>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
